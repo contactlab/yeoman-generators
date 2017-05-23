befebddcde55ca97e8e9e0c442bd0c5bc5505af6
Salvatore Laisa <salvatore.laisa@contactlab.com>
2017-05-23 10:57:34 +0200

    services file draft

--- generators/element-v2/templates/services/index.js
+++ generators/element-v2/templates/services/index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+/* Endpoints configuration imports here */
+
+// Backend data fetching configuration
+import Backend from './../libs/backend';
+
+export default {
+  getData: () => {
+    // return '';
+    return Backend(ACServices('DEV'),'/data');
+  }
+}

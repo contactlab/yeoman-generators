f57a1be6db11d0c6442b2b81b4656aa11d2f8836
gabriele.borelli <gabriele.borelli@ext.contactlab.com>
2017-10-04 16:49:26 +0200

    element-next fixes for eslint

--- generators/element-next/templates/test/index.js
+++ generators/element-next/templates/test/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// eslint-disable-next-line
 process.env.NODE_ENV = 'test';
 
 import test from 'ava';
@@ -8,6 +9,6 @@ import props from './../props';
 const element = '<%= elementName %>';
 
 test(`${element} properties: types & default values`, t => {
-    t.is(props.prop.type, Boolean);
-    t.is(props.prop.value, false);
+  t.is(props.prop.type, Boolean);
+  t.is(props.prop.value, false);
 });

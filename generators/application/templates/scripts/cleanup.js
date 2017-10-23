447737ed65036af0d6e0a1ae4efdfea8bb0506ee
StefanoMagrassi <stefano.magrassi@gmail.com>
2017-10-23 16:10:50 +0200

    no bower in cleanup

--- generators/application/templates/scripts/cleanup.js
+++ generators/application/templates/scripts/cleanup.js
@@ -2,12 +2,11 @@
 
 const fs                      = require('fs-extra');
 const {start, succeed, error} = require('./helpers/pipeline');
-const {BOWER, VENDOR, DIST}   = require('./helpers/folders');
+const {VENDOR, DIST}          = require('./helpers/folders');
 
 module.exports = () =>
   start('Cleaning up')
     .then(() => fs.remove(VENDOR))
-    .then(() => fs.remove(BOWER))
     .then(() => fs.remove(DIST))
     .then(succeed('Project cleaned up'))
     .catch(error);
--- generators/application/templates/scripts/helpers/folders.js
+++ generators/application/templates/scripts/helpers/folders.js
@@ -7,7 +7,6 @@ const root = path.resolve(__dirname, '../..');
 module.exports = {
   ROOT   : root,
   MODULES: path.join(root, 'node_modules'),
-  BOWER  : path.join(root, 'bower_components'),
   SRC    : path.join(root, 'src'),
   VENDOR : path.join(root, 'src/vendor'),
   DIST   : path.join(root, 'dist')
